fa1f9b4936019593966f4fc459c81cd02f01a83d
husteem <muhdnaimhussainl@example.com>
2026-03-25 20:56:23 +0100

    Completed week 2 exercises

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,7 @@
 # Create a SegWit address.
 # Add funds to the address.
 # Return only the Address
+
+address=$(bitcoin-cli -regtest getnewaddress "" "bech32")
+bitcoin-cli -regtest generatetoaddress 101 "$address" > /dev/null
+echo "$address"
